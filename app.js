4883557ee24c1e3abd1b05c024787e31886cceb2
Abel  Vilca <abel.vilca.1212@gmail.com>
2025-01-17 10:23:37 -0500

    Update app.js
    Se agrego else y cambio de numerosecreto

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-let numeroSecreto = 60;
+let numeroSecreto = 12;
 let numeroUsuario = prompt("Me indicas un numero por favor:");
 
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
     alert('Acertaste el numero');
+}
+else{
+    alert('Numero equivocado, intentalo de nuevo');
 }
