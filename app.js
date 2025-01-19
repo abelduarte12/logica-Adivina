482ef17d549697cc68395f52fb59e32345ddbf7e
Abel  Vilca <abel.vilca.1212@gmail.com>
2025-01-19 08:50:39 -0500

    Update app.js
    brinda un rango y colocar valor oculto

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let numeroSecreto = 12;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
 
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+    alert(`Acertaste el numero es: ${numeroUsuario}`);
 }
 else{
     alert('Numero equivocado, intentalo de nuevo');
